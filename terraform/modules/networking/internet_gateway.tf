566ceef2a675bc3d54a1310159af1f349437e3d5
Juan David <juandapv995@gmail.com>
2026-04-18 00:24:34 -0500

    Fixing Errors From the IGW Attachment

--- terraform/modules/networking/internet_gateway.tf
+++ terraform/modules/networking/internet_gateway.tf
@@ -5,8 +5,3 @@ resource "aws_internet_gateway" "core_igw" {
         Name = "core-igw"
     }
 }
-
-resource "aws_internet_gateway_attachment" "core_igw_attachment" {
-  vpc_id = aws_vpc.core_vpc.id
-  internet_gateway_id = aws_internet_gateway.core_igw.id
-}
