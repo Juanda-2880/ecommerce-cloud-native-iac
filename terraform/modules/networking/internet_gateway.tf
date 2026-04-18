e8bb88ac45efcd202a976485f02c3619a196091f
Juan David <juandapv995@gmail.com>
2026-04-18 00:21:18 -0500

    IGW Attach to Core VPC

--- terraform/modules/networking/internet_gateway.tf
+++ terraform/modules/networking/internet_gateway.tf
@@ -1,7 +1,12 @@
-resource "aws_internet_gateway" "name" {
+resource "aws_internet_gateway" "core_igw" {
   vpc_id = aws_vpc.core_vpc.id
   region = var.region
     tags = {
         Name = "core-igw"
     }
+}
+
+resource "aws_internet_gateway_attachment" "core_igw_attachment" {
+  vpc_id = aws_vpc.core_vpc.id
+  internet_gateway_id = aws_internet_gateway.core_igw.id
 }
