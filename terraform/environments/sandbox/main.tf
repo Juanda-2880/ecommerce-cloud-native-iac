eb19e1d17b4d63b21467287acb0480e7ee264695
Juan David <juandapv995@gmail.com>
2026-04-18 22:27:24 -0500

    Adding to News Subnets Private and Public

--- terraform/environments/sandbox/main.tf
+++ terraform/environments/sandbox/main.tf
@@ -6,4 +6,8 @@ module "networking" {
     private_subnet_cidr = var.private_subnet_cidr
     public_subnet_az = var.public_subnet_az
     private_subnet_az = var.private_subnet_az
+    public_subnet_cidr_2 = var.public_subnet_cidr_2
+    private_subnet_cidr_2 = var.private_subnet_cidr_2
+    public_subnet_az_2 = var.public_subnet_az_2
+    private_subnet_az_2 = var.private_subnet_az_2
 }
