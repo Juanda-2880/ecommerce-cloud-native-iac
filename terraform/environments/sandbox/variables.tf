b849557fd29724b04f19260bdac57bdef7df5ef3
Juan David <juandapv995@gmail.com>
2026-04-19 00:20:06 -0500

    Fixing Errors

--- terraform/environments/sandbox/variables.tf
+++ terraform/environments/sandbox/variables.tf
@@ -50,13 +50,3 @@ variable "app_security_group_id" {
     type = string
     default = ""
 }
-
-variable "vpc_id" {
-    description = "VPC ID for RDS instance"
-    type = string
-}
-
-variable "subnet_ids" {
-    description = "Subnet IDs for RDS instance"
-    type = list(string)
-}
