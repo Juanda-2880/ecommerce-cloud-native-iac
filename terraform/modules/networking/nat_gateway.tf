0c832467105a215d448c75489d58e7bf6605014a
Juan David <juandapv995@gmail.com>
2026-04-18 22:22:34 -0500

    Improving NAT Gateway

--- terraform/modules/networking/nat_gateway.tf
+++ terraform/modules/networking/nat_gateway.tf
@@ -1,3 +1,8 @@
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+  tags = { Name = "core-nat-eip" }
+}
+
 resource "aws_nat_gateway" "core_nat_gateway" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id = aws_subnet.public_subnet.id
