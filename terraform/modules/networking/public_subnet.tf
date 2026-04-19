76509fba2c70f5e0fa2184d9cbb295f00e7c895a
Juan David <juandapv995@gmail.com>
2026-04-18 22:24:10 -0500

    Improving Public Subnet

--- terraform/modules/networking/public_subnet.tf
+++ terraform/modules/networking/public_subnet.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "public_subnet" {
     vpc_id = aws_vpc.core_vpc.id
     cidr_block = var.public_subnet_cidr
     availability_zone = var.public_subnet_az
+    map_public_ip_on_launch = true
     tags = {
         Name = "public-subnet"
     }
