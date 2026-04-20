a2710f510eca50b7141c19695eef67fc1efed9c7
Juan David <juandapv995@gmail.com>
2026-04-19 21:11:08 -0500

    Front End Ready to Start

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+//** @type {import('tailwindcss').Config} */
+
+import daisyui from 'daisyui'
+
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,html}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [require('daisyui')],
+    daisyui: {
+        themes: ['light'],
+    },
+}
