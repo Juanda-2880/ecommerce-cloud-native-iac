29514a9b3cdfc986c6219eb99f65fa6acab2935c
Juan David <juandapv995@gmail.com>
2026-04-21 15:18:12 -0500

    Init WIth a Little of Backend

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,7 @@
+export const getProfileService = async () => {}
+
+export const LoginService = async () => {}
+
+export const RegisterService = async () => {}
+
+export const LogoutService = async () => {}
